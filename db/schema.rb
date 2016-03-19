dfa5d2e6b87b7fa1d644132063bc7ad304dfc7d3
Patricia Shatz <patriciashatz@Patricias-MacBook.local>
2016-03-19 16:53:58 -0700

    more modifications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319222926) do
+ActiveRecord::Schema.define(version: 20160319225504) do
 
   create_table "adventures", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20160319222926) do
     t.string   "description"
     t.string   "picture"
     t.date     "visit"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "adventure_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
--- spec/factories/adventures.rb
+++ spec/factories/adventures.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :adventure do
-    visit { Date.today.strftime("%A, %B %d %I:%M %P") }
-    name { FFaker::Sport.name }
-    # location { FFaker::AddressUS.neighborhood }
-    description { FFaker::HipsterIpsum.words(4).join(',') }
+    visit Date.today.strftime("%A, %B %d %I:%M %P")
+    name FFaker::Sport.name
+    description FFaker::HipsterIpsum.words(4).join(',')
   end
 end
